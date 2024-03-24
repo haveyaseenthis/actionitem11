2ee145aa629c1d6a06ac6512cb00bf130fd4d796
root <ahmedyasin1717@gmail.com>
2024-03-24 04:46:07 +0000

    terraform main file

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "RHEL-Server" {
+  count         = 1
+  ami           = "ami-0fe630eb857a6ec83"
+  instance_type = "t2.micro"
+  key_name      = "yasinDEVOPS2"
+  tags = {
+    Name = "RHEL-Server"
+  }
+  root_block_device {
+    volume_size = 15 
+    volume_type = "gp3"
+    encrypted   = true
+  }
+
+}
+
+resource "aws_instance" "Ubuntu-Linux" {
+  count         = 1
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t2.micro"
+  key_name      = "yasinDEVOPS2"
+  tags = {
+    Name = "Ubuntu-Linux"
+  }
+}
