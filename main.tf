c90ae8f79bda5a717d2907c954753480f9ed014a
root <ahmedyasin1717@gmail.com>
2024-03-24 05:15:17 +0000

    adding changes to test webhook

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "RHEL-Server" {
+resource "aws_instance" "AI-11-RHEL-Server" {
   count         = 1
   ami           = "ami-0fe630eb857a6ec83"
   instance_type = "t2.micro"
   key_name      = "yasinDEVOPS2"
   tags = {
-    Name = "RHEL-Server"
+    Name = "AI-11-RHEL-Server"
   }
   root_block_device {
     volume_size = 15 
